5805b16ca40fbc8127139ac70e49b1b6cd1be33e
tox <tox@tox.wtf>
2026-07-10 17:32:15 -0500

    feat: add gitsigns
    Signed-off-by: tox <tox@tox.wtf>

--- lua/k.lua
+++ lua/k.lua
@@ -52,3 +52,43 @@ map("n", "<leader>fb", ts.buffers,                    { desc = "Find buffers"   
 map("n", "<leader>fd", ts.diagnostics,                { desc = "Find diagnostics"   })
 map("n", "<leader>fs", ts.lsp_document_symbols,       { desc = "Find symbols"       })
 map("n", "<leader>fp", "<cmd>Telescope projects<cr>", { desc = "Find projects"      })
+
+local gitsigns = require("gitsigns")
+map("n", "<leader>hs", gitsigns.stage_hunk,                 { desc = "Stage hunk"         })
+map("n", "<leader>hr", gitsigns.reset_hunk,                 { desc = "Reset hunk"         })
+
+map("v", "<leader>hs", function()
+    gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+end, { desc = "Stage hunk" })
+
+map("v", "<leader>hr", function()
+    gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+end, { desc = "Reset hunk" })
+
+map("n", "<leader>hS", gitsigns.stage_buffer,         { desc = "Stage buffer"       })
+map("n", "<leader>hR", gitsigns.reset_buffer,         { desc = "Reset buffer"       })
+map("n", "<leader>hp", gitsigns.preview_hunk,         { desc = "Preview hunk"       })
+map("n", "<leader>hi", gitsigns.preview_hunk_inline,  { desc = "Preview hunk inline" })
+
+map("n", "<leader>hb", function()
+    gitsigns.blame_line({ full = true })
+end, { desc = "Blame line" })
+
+map("n", "<leader>hd", gitsigns.diffthis,             { desc = "Diff this"          })
+
+map("n", "<leader>hD", function()
+    gitsigns.diffthis("~")
+end, { desc = "Diff since ~" })
+
+map("n", "<leader>hq", gitsigns.setqflist)
+
+map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+map("n", "<leader>tw", gitsigns.toggle_word_diff)
+
+map("n", "]h", function()
+    gitsigns.nav_hunk("next")
+end)
+
+map("n", "[h", function()
+    gitsigns.nav_hunk("prev")
+end)
